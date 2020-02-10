7662893101ce214d245413765819fd8ee26bb930
Dianadhu <dimidusa@gmail.com>
2020-02-10 09:20:12 -0500

    Initial commit

---
+++ app/controllers/api/v1/products_controller.rb
@@ -0,0 +1,48 @@
+module Api
+  module V1
+    class ProductsController < ApplicationController
+      def index
+        products = Product.all
+        render json: products
+      end
+
+      # def new
+      #   @product = Product.new
+      # end
+      #
+      def create
+        product = Product.new(product_params)
+        if product.save
+          render json: product, status: 201
+        else
+          render json: { errors: product.errors }, status: 422
+        end
+      end
+      #
+      # def edit
+      #   @product = Product.find(params[:id])
+      # end
+      #
+      def update
+        product = Product.find(params[:id])
+        if product.update(product_params)
+          render json: product, status: 200
+        else
+          render json: {errors: product.errors}, status:422
+        end
+      end
+
+      def destroy
+        product = Product.find(params[:id])
+        product.destroy
+        head :no_content
+      end
+
+      private
+        def product_params
+          params.require(:product).permit(:name, :price)
+        end
+    end
+
+  end
+end
